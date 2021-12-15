bc7c2ef94e75058faf5498488c35a17a6b003547
nununuam <nunusnuam@gmail.com>
2021-12-15 15:15:36 -0800

    Starting celebs data are created

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 
+const Celebrities= [{name:'Benedict Cumberbatch', 
+                        age:'45', 
+                        height:`6'0"`, 
+                        funFact:'Play Sherlock', 
+                        status:'married', 
+                        img:'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgxMDEwMDE1MDUyNjM3NTQ0/cumberbatch_179663904jpg.jpg'},
+
+                    {name:'Chris Hemsworth', 
+                        age:'38', 
+                        height:`6'3"`, 
+                        funFact:'Australian', 
+                        status:'married',
+                        img:'https://www.the-sun.com/wp-content/uploads/sites/6/2021/05/c81b7b96-3fea-47b0-bc67-c695c75f0a95.jpg' },
+
+                    {name:'Will Smith', 
+                        age:'53', 
+                        height:`6'2"`, 
+                        funFact:"Can solve Rubik's Cube in 55 seconds", 
+                        status:'married', 
+                        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL0tz9q7U2dVBL6qqp-A-WDMyktyZSAJdavwiUcGEB7YkXYwo0p2E3BYAGVbhMcywT6b4&usqp=CAU' },
+
+                    {name:'Selena Gomez', 
+                        age:'29', 
+                        height:`5'5"`, 
+                        funFact:'Play Gianna on Barney', 
+                        status:'single', 
+                        img:'https://www.bollywoodhungama.com/wp-content/uploads/2021/12/Selena-Gomez-invests-in-15-billion-grocery-delivery-company-Gopuff-1.jpg' },
+
+                    {name:'Jennifer Aniston', 
+                        age:'52', 
+                        height:`5'5"`, 
+                        funFact:'From Sherman Oaks, CA', 
+                        status:'single', 
+                        img:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/ja3-longform-lead-1544370437.jpg?crop=0.752xw:1.00xh;0.125xw,0&resize=640:*' },
+
+                    {name:'Jennifer Lopez', 
+                        age:'52', 
+                        height:`5'5"`, 
+                        funFact:'From The Bronx, NY', 
+                        status:'single',
+                        img:'https://www.lifeandstylemag.com/wp-content/uploads/2016/09/jennifer-lopez.jpg' },
+]
 
 
 
+let peopleKiiled = [];
+let peoppleKissed = [];
+let peopleMarried = [];
 
-//Replace the buttons when click start game
-const replaceStartButton = function (){
-    ('#startButton').on('click', function(){
+let numsClickedTraker = 0;
 
-    });
-}
+let randomTime = Math.floor(Math.random(2)*7)
 
 
+
+//WebPage section:
 $('#startButton').on('click', function(a){
     a.preventDefault();
     //take away the howtoplay intructions
@@ -21,13 +65,10 @@ $('#startButton').on('click', function(a){
     $('#startButton').attr('id', 'exitButton')
     $('#exitButton').html('Exit');
     exitButton();
-    theProfile();
-    addGameButtons();
-    kissButton();
-    killButton();
-    marryButton();
-
-
+    
+    gameContinue();
+        
+   
     
 });
 
@@ -37,6 +78,18 @@ $('#howToPlayButton').on('click', function(b){
 
     $('.gameArea').html('<p class ="instruction">Every turn you will get a profile. You get to pick if you want to kiss kill or marry them by clicking the button. You have certain amount of time allocated before the game end. The time allocated is randomize. Who every you choose to marry at the end of the game will reveal if you actually ended up marrying them. Remember, whoever you choose to marry last at the end of the round is who you will see if it works out.</p>');
 });
+
+let gameContinue = function(){
+    if(numsClickedTraker<=4){
+        theProfile();
+        addGameButtons();
+        kissButton();
+        killButton();
+        marryButton();
+    }else{
+
+    }
+}
 
 const exitButton = function(){
     $('#exitButton').on('click', function(c){
@@ -52,11 +105,14 @@ const theProfile = function(){
 const addGameButtons = function(){
     $('.gameArea').append('<button id="killButton">Kill</button><button id="kissButton">Kiss</button><button id="marryButton">Marry</button>')
 }
+
 const killButton= function(){
     $('#killButton').on('click', function(d){
         d.preventDefault();
         
         console.log('killButton works')
+        numsClickedTraker += 1;
+        console.log(numsClickedTraker);
     })
 };
 const kissButton = function(){
@@ -64,6 +120,8 @@ const kissButton = function(){
         e.preventDefault();
 
         console.log('kissButton works')
+        numsClickedTraker += 1;
+        console.log(numsClickedTraker);
     })
 }
 const marryButton = function(){
@@ -71,11 +129,16 @@ const marryButton = function(){
         f.preventDefault();
 
         console.log('marryButton woks')
+        numsClickedTraker += 1;
+        console.log(numsClickedTraker);
     })
 }
 const marriedProfile = function(){
     $('.marry').html(`<img id = 'marryProfile' src="https://cdn.pixabay.com/photo/2018/05/21/04/21/animal-3417350__480.jpg"`);
 }
 const endResult = function(){
+
     $('.endGameResult').append(`<p>1</p><p>2</p><p>3</p>`);
 }
+
+
